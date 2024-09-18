c99a3d6daea8482006688665e6e765f8ab7470ef
Hardik Advani <127594594+hargit19@users.noreply.github.com>
2024-09-18 10:53:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
 app.use(cors({
-    origin: 'http://localhost:3000',// Frontend URL in development
+    origin: '',// Frontend URL in development
     methods: ["POST" , "GET"], 
     credentials: true,
 }));
